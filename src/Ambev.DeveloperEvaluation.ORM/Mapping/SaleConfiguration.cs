acf053d734c0369f4db2210cfd31bf567cc20763
Hector <hectorwender7@gmail.com>
2026-01-20 19:44:00 -0300

    FEAT: Configuration mapping - Sale anda SaleItem

---
+++ src/Ambev.DeveloperEvaluation.ORM/Mapping/SaleConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Ambev.DeveloperEvaluation.ORM.Mapping
+{
+    public class SaleConfiguration : IEntityTypeConfiguration<Sale>
+    {
+        public void Configure(EntityTypeBuilder<Sale> builder)
+        {
+            builder.ToTable("Sales");
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.SaleNumber).IsRequired().HasMaxLength(50);
+            builder.Property(x => x.TotalAmount).HasColumnType("decimal(18,2)");
+            builder.Property(x => x.Status).HasConversion<int>(); // sAVE AS 0, 1, 2
+            
+            // 1:N
+            builder.HasMany(x => x.Items)
+                .WithOne()
+                .HasForeignKey(x => x.SaleId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
---
+++ src/Ambev.DeveloperEvaluation.ORM/Mapping/SaleItemConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Ambev.DeveloperEvaluation.ORM.Mapping
+{
+    public class SaleItemConfiguration : IEntityTypeConfiguration<SaleItem>
+    {
+        public void Configure(EntityTypeBuilder<SaleItem> builder)
+        {
+            builder.ToTable("SaleItems");
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.UnitPrice).HasColumnType("decimal(18,2)");
+            builder.Property(x => x.Discount).HasColumnType("decimal(18,2)");
+            builder.Property(x => x.TotalAmount).HasColumnType("decimal(18,2)");
+        }
+    }
+}

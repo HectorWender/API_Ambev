5dad95bceef3ffbdf00529f6883c9e804017c51f
Hector <hectorwender7@gmail.com>
2026-01-20 18:43:11 -0300

    BUILD: First project build

---
+++ src/Ambev.DeveloperEvaluation.Domain/Repositories/ISaleRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Ambev.DeveloperEvaluation.Domain.Entities;
+
+namespace Ambev.DeveloperEvaluation.Domain.Repositories
+{
+    public interface ISaleRepository
+    {
+        Task<Sale> CreateAsync(Sale sale, CancellationToken cancellationToken = default);
+        Task<Sale?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+        Task UpdateAsync(Sale sale, CancellationToken cancellationToken = default);
+        Task<bool> DeleteAsync(Guid id, CancellationToken cancellationToken = default);
+    }
+}

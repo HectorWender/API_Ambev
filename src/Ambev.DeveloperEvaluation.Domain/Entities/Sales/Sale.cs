4104980e359b39bb00c2c4986615ef33eec31007
Hector <hectorwender7@gmail.com>
2026-01-20 18:44:51 -0300

    FEAT: Create Domain Classes

---
+++ src/Ambev.DeveloperEvaluation.Domain/Entities/Sales/Sale.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Ambev.DeveloperEvaluation.Domain.Common;
+
+namespace Ambev.DeveloperEvaluation.Domain.Entities
+{
+    public class Sale : BaseEntity
+    {
+        public string SaleNumber { get; set; }
+        public DateTime SaleDate { get; set; }
+        public Guid CustomerId { get; set; }
+        public string CustomerName { get; set; }
+        public Guid BranchId { get; set; }
+        public string BranchName { get; set; } 
+        public bool IsCancelled { get; set; }
+        public decimal TotalAmount { get; set; }
+
+        public ICollection<SaleItem> Items { get; set; } = new List<SaleItem>();
+
+        public void CalculateTotal()
+        {
+            TotalAmount = Items.Sum(i => i.TotalAmount);
+        }
+
+        public void Cancel()
+        {
+            IsCancelled = true;
+            foreach (var item in Items)
+            {
+                item.Cancel();
+            }
+        }
+    }
+}
---
+++ src/Ambev.DeveloperEvaluation.Domain/Entities/Sales/SaleItem.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Ambev.DeveloperEvaluation.Domain.Common;
+
+namespace Ambev.DeveloperEvaluation.Domain.Entities
+{
+    public class SaleItem : BaseEntity
+    {
+        public Guid SaleId { get; set; }
+        public Guid ProductId { get; set; }
+        public string ProductName { get; set; }
+        public int Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal Discount { get; set; }
+        public decimal TotalAmount { get; set; }
+        public bool IsCancelled { get; set; }
+
+        public void ApplyDiscountRules()
+        {
+            if (Quantity > 20) throw new InvalidOperationException("Cannot sell more than 20 identical items.");
+
+            if (Quantity < 4) Discount = 0;
+            else if (Quantity is >= 4 and < 10) Discount = (UnitPrice * Quantity) * 0.10m; // discount 10%
+            else if (Quantity is >= 10 and <= 20) Discount = (UnitPrice * Quantity) * 0.20m; // 20%
+
+            TotalAmount = (UnitPrice * Quantity) - Discount;
+        }
+
+        public void Cancel()
+        {
+            IsCancelled = true;
+        }
+    }
+}
